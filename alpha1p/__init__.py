43184290094306311320793a96aea85c773297ff
wuruiqi <woshiwuruiqi@163.com>
2020-04-14 22:14:00 +0800

    v 0.1

---
+++ alpha1p/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .version import version as __version__
+from . import robot  # pylint: disable=wildcard-import
+from . import dance # pylint: disable=wildcard-import
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup, find_packages
+
+# with open('README.md', 'r') as fdesc:
+#     long_description = fdesc.read()
+
+setup(
+    name = "alpha1p",
+    version = "0.1",
+    # packages=['alpha1p'],
+    packages = find_packages(),
+    # packages = find_packages('alpha1p'),
+    # package_dir = {'':'alpha1p'},
+    # package_data={'': ['Datas/*']},
+
+    # include_package_data=True,
+    # platforms="any",
+	install_requires = ['numpy>=1.18.1', 'pandas>=1.0.1', 'numba>=0.48.0', 'pyusb>=1.0.2', 'pyserial>=3.4'],
+    # metadata for upload to PyPI
+    author = "wuruiqi",
+    author_email = "woshiwuruiqi@163.com",
+    description = "Alpha 1 Pro driver",
+    # long_description=long_description,
+    # long_description_content_type='text/markdown',
+    license = "GNU General Public License v3.0",
+    keywords = ["Alpha 1", "ubteach"],
+    url="https://github.com/wuruiqi/alpha1p",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+    classifiers=[           # 程序的所属分类列表
+        "Programming Language :: Python :: 3",
+        "License :: GPL Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",        
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+
+
+    zip_safe=False
+)
